e8e60b1c440eafd1fc59381c4e26ac4b306661f0
amir7896 <amirshahzad07896@gmail.com>
2025-01-23 11:05:40 -0500

    Posts screen created

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import axios from 'axios';
-
-type Post = {
-  id: number;
-  title: string;
-  body: string;
-};
-
-type SectionProps = {
-  title: string;
-  children: React.ReactNode;
-};
-
-function Section({children, title}: SectionProps): React.ReactElement {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>{title}</Text>
-      <Text style={styles.sectionDescription}>{children}</Text>
-    </View>
-  );
-}
+import React from 'react';
+import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import Posts from './screens/posts/post';
 
 function App(): React.ReactElement {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        setPosts(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={styles.container}>
-        <View style={styles.container}>
-          <Section title="Posts">
-            {posts.map(post => (
-              <View key={post.id} style={styles.card}>
-                <Text style={styles.postId}>ID: {post.id}</Text>
-                <Text style={styles.postTitle}>{post.title}</Text>
-                <Text style={styles.postBody}>{post.body}</Text>
-              </View>
-            ))}
-          </Section>
-        </View>
+        <Posts />
       </ScrollView>
     </SafeAreaView>
   );
@@ -69,47 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F8F8F8',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  card: {
-    backgroundColor: 'white',
-    borderRadius: 8,
-    padding: 16,
-    marginVertical: 12, // Increased vertical margin here
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  postId: {
-    fontSize: 16,
-    fontWeight: '400',
-    color: '#333',
-  },
-  postTitle: {
-    fontSize: 20,
-    fontWeight: '600',
-    marginVertical: 4,
-  },
-  postBody: {
-    fontSize: 16,
-    color: '#666',
   },
 });
 
---
+++ src/screens/posts/post.tsx
@@ -0,0 +1,101 @@
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import axios from 'axios';
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+type SectionProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+function Section({children, title}: SectionProps): React.ReactElement {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>{title}</Text>
+      <Text style={styles.sectionDescription}>{children}</Text>
+    </View>
+  );
+}
+
+function Posts(): React.ReactElement {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Section title="Posts">
+        {posts.map(post => (
+          <View key={post.id} style={styles.card}>
+            <Text style={styles.postId}>ID: {post.id}</Text>
+            <Text style={styles.postTitle}>{post.title}</Text>
+            <Text style={styles.postBody}>{post.body}</Text>
+          </View>
+        ))}
+      </Section>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 16,
+    marginVertical: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  postId: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: '#333',
+  },
+  postTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginVertical: 4,
+  },
+  postBody: {
+    fontSize: 16,
+    color: '#666',
+  },
+});
+
+export default Posts;
