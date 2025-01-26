b5613784f31753718f09c1d89088f6ab5f2f3717
amir7896 <amirshahzad07896@gmail.com>
2025-01-26 18:34:01 +0500

    Auth header globally set

--- src/services/client/rest.ts
+++ src/services/client/rest.ts
@@ -1,10 +1,31 @@
 import axios, {AxiosInstance} from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Ip: string = '192.168.45.180';
 const Port: string = '4000';
 
 export const Api: AxiosInstance = axios.create({
   baseURL: `http://${Ip}:${Port}/api/`,
-  // baseURL: `${deployUrl}api/`,
   headers: {'Content-type': 'application/json'},
 });
+
+// Add a request interceptor to include the Authorization header
+Api.interceptors.request.use(
+  async config => {
+    try {
+      // Retrieve the token from AsyncStorage
+      const token = await AsyncStorage.getItem('userToken');
+      if (token) {
+        // If token exists, add it to the Authorization header
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error('Error retrieving token from AsyncStorage:', error);
+    }
+    return config;
+  },
+  error => {
+    // Handle request error
+    return Promise.reject(error);
+  },
+);
