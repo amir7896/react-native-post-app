72e96c2e8ee34403fd6fcbd0a1d63f3688bd97ff
amir7896 <amirshahzad07896@gmail.com>
2025-01-28 18:04:53 -0500

    Icons added in bottom tab

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -3,3 +3,7 @@ export {default as CommentIcon} from './comments.svg';
 export {default as AddIcon} from './add.svg';
 export {default as CancelIcon} from './cancel.svg';
 export {default as LikeFilledIcons} from './like-filled.svg';
+export {default as HomeOutLined} from './home-outlined.svg';
+export {default as HomeFilled} from './home-filled.svg';
+export {default as PostOutLined} from './post-outlined.svg';
+export {default as PostFilled} from './post-filled.svg';
