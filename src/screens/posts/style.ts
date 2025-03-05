2d634c02e7d2a79eb3ca684409e1f8c9a4c807ce
amir7896 <amirshahzad07896@gmail.com>
2025-03-05 14:58:20 +0500

    Change ui of post done

--- src/screens/posts/style.ts
+++ src/screens/posts/style.ts
@@ -1,152 +1,111 @@
 'use strict';
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  // Render post container style
   container: {
-    paddingHorizontal: 24,
-    paddingVertical: 32,
+    paddingHorizontal: 0, // Remove horizontal padding
+    paddingVertical: 8, // Add some vertical padding
+    backgroundColor: '#f0f2f5', // Facebook background color
   },
-  // Card style
   card: {
     backgroundColor: 'white',
-    borderRadius: 8,
-    padding: 16,
-    marginVertical: 12,
+    padding: 12,
+    marginVertical: 4, // Reduce margin between cards
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 1,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    shadowOpacity: 0.18,
+    shadowRadius: 1.00,
+    elevation: 1,
   },
-  // Top Section Style (Profile + User Info)
   topSection: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 10,
   },
-  // User Profile Card style
   userProfileCard: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: '#e0e0e0',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#ccc',
+    width: 40, // Reduced profile image size
+    height: 40,
+    borderRadius: 20,
     overflow: 'hidden',
     marginRight: 10,
   },
-  userProfileName: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#333',
+  userProfileImage: {
+    width: '100%',
+    height: '100%',
   },
-
-  // User Info Style (Username and Date - Vertical Layout)
   userInfo: {
     flexDirection: 'column',
     justifyContent: 'center',
   },
-
-  userProfileImage: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 25,
-  },
-  // Username Style (next to profile circle)
   userName: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#333',
+    fontWeight: '600',
+    color: '#050505', // Facebook username color
     marginBottom: 2,
   },
-  // Post Date Style (below username)
   postDate: {
-    fontSize: 14,
-    color: '#777',
+    fontSize: 12,
+    color: '#606770', // Facebook date color
   },
-  // Post title style
+  postContent: {
+    marginBottom: 12,
+  },
   postTitle: {
-    fontSize: 20,
-    fontWeight: '600',
-    marginVertical: 4,
-  },
-  // Post body style
-  postBody: {
     fontSize: 16,
-    color: '#666',
+    fontWeight: '500',
+    marginBottom: 4,
+    color: '#050505'
   },
-
-  // Like and comment section container
+  postBody: {
+    fontSize: 14,
+    color: '#050505',
+    lineHeight: 20,
+  },
   likeCommentSection: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    marginTop: 14,
+    paddingTop: 8,
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
   },
-
-  // Like button style
   likeButton: {
     flexDirection: 'row',
-    width: 100,
-    paddingVertical: 6,
-    paddingHorizontal: 8,
-    borderRadius: 16,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#E4E6EB',
-    borderWidth: 1,
-    borderColor: '#BCC0C0',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 18,
   },
-
-  // Like button text
   likeButtonText: {
-    color: '#4C4F56',
-    fontWeight: '600',
     fontSize: 14,
+    color: '#606770',
     marginLeft: 6,
   },
-
-  // Comment button style
   commentButton: {
     flexDirection: 'row',
-    width: 100,
-    paddingVertical: 6,
-    paddingHorizontal: 8,
-    borderRadius: 16,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F0F2F5',
-    borderWidth: 1,
-    borderColor: '#BCC0C0',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 18,
   },
-
-  // Add post container
+  commentButtonText: {
+    fontSize: 14,
+    color: '#606770',
+    marginLeft: 6,
+  },
+  deleteButton: {
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 18,
+  },
   addPostContainer: {
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
     paddingTop: 15,
     paddingHorizontal: 25,
-  },
-  // Comment button text
-
-  // Delete post button
-  deleteButton: {
-    flexDirection: 'row',
-    width: 100,
-    paddingVertical: 6,
-    paddingHorizontal: 8,
-    borderRadius: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F0F2F5',
-    borderWidth: 1,
-    borderColor: '#BCC0C0',
   },
 });
